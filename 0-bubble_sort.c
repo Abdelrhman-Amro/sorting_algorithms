fd449d2762b00085ae298abe424fa36146f7a925
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-21 15:42:02 +0200

    fix betty in bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,24 +8,24 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    int sorted, temp = 0;
-    size_t nsteps, idx;
+	int sorted, temp = 0;
+	size_t nsteps, idx;
 
-    for (nsteps = 1; nsteps < size; nsteps++)
-    {
-        sorted = 1;
-        for (idx = 0; idx < (size - nsteps); idx++)
-        {
-            if (array[idx] > array[idx + 1])
-            {
-                temp = array[idx];
-                array[idx] = array[idx + 1];
-                array[idx + 1] = temp;
-                sorted = 0;
-                print_array(array, size);
-            }
-        }
-        if (sorted)
-            break;
-    }
+	for (nsteps = 1; nsteps < size; nsteps++)
+	{
+		sorted = 1;
+		for (idx = 0; idx < (size - nsteps); idx++)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+				temp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = temp;
+				sorted = 0;
+				print_array(array, size);
+			}
+		}
+		if (sorted)
+			break;
+	}
 }
