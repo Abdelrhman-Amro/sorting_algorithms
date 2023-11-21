57413770c5ab75f964c9230cc3a2d55793ac5124
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-21 18:15:48 +0200

    completed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,8 +8,8 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int min_idx, tmp;
+	size_t i, j, min_idx;
+	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -19,9 +19,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
-		tmp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = tmp;
-		print_array(array, size);
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
 	}
 }
